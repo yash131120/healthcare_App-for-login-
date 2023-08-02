32533958d0e58f1991e2107b891d56cb177f1374
yash131120 <139432375+yash131120@users.noreply.github.com>
2023-08-02 17:50:00 +0530

    1st project in collage

---
+++ app/src/main/java/com/example/healthcare/HomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username","").toString();
+        Toast.makeText(getApplicationContext(), "Welcome "+username, Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.card);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,WorkerActivity.class));
+            }
+        });
+    }
+}
